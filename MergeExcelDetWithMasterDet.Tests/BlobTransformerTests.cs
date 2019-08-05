9a45aec4c84e8916aa6b1aa4279433dd993abd78
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-08-05 10:40:16 -0700

    Updated harvest DET to newest draft and added more tests

--- MergeExcelDetWithMasterDet.Tests/BlobTransformerTests.cs
+++ MergeExcelDetWithMasterDet.Tests/BlobTransformerTests.cs
@@ -90,14 +90,17 @@ namespace IngestExcelDetTest.Tests
         }
 
         [Fact]
-        public async void MergeBlobs_ChangeParameterValue_UpdatesValue()
+        public async void MergeBlobs_ChangeParameterValue_UpdatesParamValueOnly()
         {
             // Arrange
             Mock<IBlobExtractor> extractor = new Mock<IBlobExtractor>();
             extractor.Setup(l => l.ExtractBlobAsync(It.IsAny<string>()))
                 .Returns((string input) => Task.FromResult<MemoryStream>(
                     GetMemoryStreamFromFile(input)));
+            int expectedParamValue = 4;
+            string expectedNonParamValue = "CE1_GP_2019_WW_Bio";
 
+            // Has change in parameter value and observation value, should only update parameter
             var det = await extractor.Object.ExtractBlobAsync(
                "Assets/Harvest01_2019_GP-ART-Lime_INT_YYYYMMDD_20190802_updateParam.xlsm");
             var master = await extractor.Object.ExtractBlobAsync(
@@ -112,9 +115,68 @@ namespace IngestExcelDetTest.Tests
             using (ExcelPackage package = new ExcelPackage(ms))
             {
                 ExcelWorksheet ws = package.Workbook.Worksheets[1];
-                Assert.True(ws.Cells[2, 2].GetValue<int>() == 4);
+                Assert.True(
+                    ws.Cells[2, 2].GetValue<int>() == expectedParamValue);
+                Assert.True(
+                    ws.Cells[8, 2].GetValue<string>() == expectedNonParamValue);
             }
-            
+        }
+
+        [Fact]
+        public async void MergeBlobs_ChangeValues_UpdatesValues()
+        {
+            // Arrange
+            Mock<IBlobExtractor> extractor = new Mock<IBlobExtractor>();
+            extractor.Setup(l => l.ExtractBlobAsync(It.IsAny<string>()))
+                .Returns((string input) => Task.FromResult<MemoryStream>(
+                    GetMemoryStreamFromFile(input)));
+            string expectedDate = "08/05/2019";
+            string expectedTotalWeight = "20";
+            string expectedInitials = "brc";
+            string expectedAvgBagMass = "1.0";
+            string expectedSecondBagMass = "N/A";
+            string expectedAvgEmptyBagMass = "2.0";
+            string expectedBiomass = "19";
+            string expectedComment = "Foo";
+
+            // Has change in parameter value and observation value, should only update parameter
+            var det = await extractor.Object.ExtractBlobAsync(
+               "Assets/Harvest01_2019_GP-ART-Lime_INT_YYYYMMDD_20190802_updateValues.xlsm");
+            var master = await extractor.Object.ExtractBlobAsync(
+                "Assets/Harvest01_2019_GP-ART-Lime_INT_YYYYMMDD_20190802.xlsm");
+
+            BlobTransformer sut = new BlobTransformer();
+
+            // Act
+            MemoryStream ms = sut.MergeBlobs(det, master, 7);
+
+            // Assert
+            using (ExcelPackage package = new ExcelPackage(ms))
+            {
+                ExcelWorksheet ws = package.Workbook.Worksheets[1];
+                Assert.True(
+                    ws.Cells[8, 3].GetValue<string>() == expectedDate);
+                Assert.True(
+                    ws.Cells[10, 3].GetValue<string>() == expectedDate);
+                Assert.True(
+                    ws.Cells[12, 3].GetValue<string>() == expectedDate);
+                Assert.True(
+                    ws.Cells[830, 3].GetValue<string>() == expectedDate);
+                Assert.True(
+                    ws.Cells[10, 4].GetValue<string>() == expectedTotalWeight);
+                Assert.True(
+                    ws.Cells[10, 5].GetValue<string>() == expectedInitials);
+                Assert.True(
+                    ws.Cells[10, 6].GetValue<string>() == expectedAvgBagMass);
+                Assert.True(
+                    ws.Cells[10, 7].GetValue<string>() == expectedSecondBagMass);
+                Assert.True(
+                    ws.Cells[10, 8].GetValue<string>() == expectedAvgEmptyBagMass);
+                Assert.True(
+                    ws.Cells[10, 9].GetValue<string>() == expectedBiomass);
+                Assert.True(
+                    ws.Cells[830, 70].GetValue<string>() == expectedComment);
+            }
         }
 
         private MemoryStream GetMemoryStreamFromFile(string filePath)
